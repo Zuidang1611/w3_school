3d6af7d00870e3cb3b793d1b11cd9a8c5a9398d4
Zuidang1611 <dangducduy1611@gmail.com>
2022-06-01 18:43:16 +0200

    save file

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,32 +1,32 @@
-
 var header = document.getElementById('header');
 var mobileMenu = document.getElementById('mobile-menu');
 var headerHeight = header.clientHeight;
 
 // open/closed mobile menu
-mobileMenu.onclick = function() {
-    var isClosed = header.clientHeight === headerHeight;
-    if (isClosed) {
-        header.style.height = "auto";
-    } else {
-        header.style.height = null;
-    }
-}
+mobileMenu.onclick = function () {
+  var isClosed = header.clientHeight === headerHeight;
+  if (isClosed) {
+    header.style.height = 'auto';
+  } else {
+    header.style.height = null;
+  }
+};
 
-// Auto close menu 
-
+// Auto close menu
 var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
 for (var i = 0; i < menuItems.length; i++) {
-    var menuItem = menuItems[i];
-    
-    menuItem.onclick = function(event) {
-        var isParentMenu = this.nextElementSibling && this.nextElementSibling.classList.contains('subnav');
-        if (isParentMenu) {
-            event.preventDefault();
-        } else {
-            header.style.height = null;
-        }
+  var menuItem = menuItems[i];
+
+  menuItem.onclick = function (event) {
+    var isParentMenu =
+      this.nextElementSibling &&
+      this.nextElementSibling.classList.contains('subnav');
+    if (isParentMenu) {
+      event.preventDefault();
+    } else {
+      header.style.height = null;
     }
+  };
 }
 
 const buyBtns = document.querySelectorAll('.js-buy-ticket');
@@ -35,19 +35,19 @@ const modalContainer = document.querySelector('.js-modal-container');
 const modalClose = document.querySelector('.js-modal-close');
 
 function showBuyTickets() {
-    modal.classList.add('open');
+  modal.classList.add('open');
 }
 
 function hideBuyTickets() {
-    modal.classList.remove('open');
+  modal.classList.remove('open');
 }
 
 for (const buyBtn of buyBtns) {
-    buyBtn.addEventListener('click', showBuyTickets);
+  buyBtn.addEventListener('click', showBuyTickets);
 }
 
 modalClose.addEventListener('click', hideBuyTickets);
 modal.addEventListener('click', hideBuyTickets);
-modalContainer.addEventListener('click', function() {
-    event.stopPropagation();
-})
+modalContainer.addEventListener('click', function () {
+  event.stopPropagation();
+});
